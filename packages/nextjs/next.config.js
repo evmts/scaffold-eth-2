1e51770b72a3aed8247bb404b69a56e2d2e34a39
Will Cory <willcory@Wills-MacBook-Pro.local>
2023-09-04 12:00:23 -0700

    feat: Add EVMts

--- packages/nextjs/next.config.js
+++ packages/nextjs/next.config.js
@@ -1,15 +1,18 @@
 // @ts-check
+const { webpackPluginEvmts } = require('@evmts/webpack-plugin')
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   typescript: {
-    ignoreBuildErrors: process.env.NEXT_PUBLIC_IGNORE_BUILD_ERROR === "true",
+    // Typechecking in NEXT.js is not yet supported but planned
+    ignoreBuildErrors: true,
   },
   eslint: {
     ignoreDuringBuilds: process.env.NEXT_PUBLIC_IGNORE_BUILD_ERROR === "true",
   },
   webpack: config => {
+    config.plugins.push(webpackPluginEvmts())
     config.resolve.fallback = { fs: false, net: false, tls: false };
     return config;
   },
